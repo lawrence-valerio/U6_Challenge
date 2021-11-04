1c2584bdb75bdbcd051f51981fecf58fdd6a7c05
Lawrence Valerio <valerio.lawrence@gmail.com>
2021-11-04 17:59:48 -0500

    Created routes for the new views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'customers#index'
-  get 'customers/index'
+  get '/customers/missing_email', to: 'customers#missing_email'
+  get '/customers/alphabetized', to: 'customers#alphabetized'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
